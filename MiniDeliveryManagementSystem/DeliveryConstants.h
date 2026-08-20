0961f32858174385a3c72d0accfd817188e86c42
CRISTIAN <crpopa1@sheffield.ac.uk>
2026-08-20 23:21:37 +0100

    Add DeliveryConstants class

---
+++ MiniDeliveryManagementSystem/DeliveryConstants.h
@@ -0,0 +1,5 @@
+#pragma once
+#include "DeliveryType.h"
+
+static const DeliveryType STANDARD_DELIVERY_TYPE = DeliveryType::STANDARD;
+static const DeliveryType EXPRESS_DELIVERY_TYPE = DeliveryType::EXPRESS;
