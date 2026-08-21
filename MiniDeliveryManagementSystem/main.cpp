26db7810585c1667f098f31f48ec81983faf4fce
CRISTIAN <crpopa1@sheffield.ac.uk>
2026-08-21 01:55:26 +0100

    Update main to test Delivery factory method.

--- MiniDeliveryManagementSystem/main.cpp
+++ MiniDeliveryManagementSystem/main.cpp
@@ -1,9 +1,40 @@
 // MiniDeliveryManagementSystem.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 #include <iostream>
+#include "DeliveryCreator.h"
+#include "DeliveryDispatcher.h"
 
 int main()
 {
+    Delivery* delivery = nullptr;
+
+    DeliveryCreator* creator = DeliveryDispatcher::getCreator(DeliveryType::STANDARD);
+
+
+    std::string id = "10001";
+    std::string customerName = "George Popovici";
+    std::string sourceCity = "Vienna";
+    std::string destinationCity = "Cracovia";
+    double weight = 2;
+    Status status = Status::IN_TRANSIT;
+
+    if (creator != nullptr)
+    {
+        delivery = creator->getDelivery(
+            id,
+            customerName,
+            sourceCity,
+            destinationCity,
+            weight,
+            status
+        );
+    }
+
+    std::cout << delivery->getId() << "\n";
+    std::cout << delivery->getCustomerName() << "\n";
+    std::cout << delivery->getSourceCity() << "\n";
+    std::cout << delivery->getDestinationCity() << "\n";
+    std::cout << delivery->getWeight() << " kg\n";
 
     return 0;
 }
