ae1b2d272141514d4563ee4e3e78ab882f7ddac9
CRISTIAN <crpopa1@sheffield.ac.uk>
2026-08-21 01:47:26 +0100

    Add Express Delivery Concrete Product constructors and destructor using rule of 4

--- MiniDeliveryManagementSystem/ExpressDelivery.cpp
+++ MiniDeliveryManagementSystem/ExpressDelivery.cpp
@@ -1,6 +1,59 @@
 #include "ExpressDelivery.h"
 #include "DeliveryConstants.h"
 
+
+ExpressDelivery::ExpressDelivery(
+    std::string idParam,
+    std::string customerNameParam,
+    std::string sourceCityParam,
+    std::string destinationCityParam,
+    double weightParam,
+    Status statusParam
+) :
+    Delivery(
+        idParam,
+        customerNameParam,
+        sourceCityParam,
+        destinationCityParam,
+        weightParam,
+        statusParam
+    )
+{
+}
+
+ExpressDelivery::ExpressDelivery(const ExpressDelivery& other):
+    Delivery(
+        other.getId(),
+        other.getCustomerName(),
+        other.getSourceCity(),
+        other.getDestinationCity(),
+        other.getWeight(),
+        other.getStatus()
+    )
+{
+}
+
+ExpressDelivery& ExpressDelivery::operator=(const ExpressDelivery& other)
+{
+    if (this != &other)
+    {
+        setId(other.getId());
+        setCustomerName(other.getCustomerName());
+        setSourceCity(other.getSourceCity());
+        setDestinationCity(other.getDestinationCity());
+        setWeight(other.getWeight());
+        setStatus(other.getStatus());
+    }
+
+    return *this;
+}
+
+
+ExpressDelivery::~ExpressDelivery()
+{
+}
+
+
 DeliveryType ExpressDelivery::getDeliveryType() const
 {
     return EXPRESS_DELIVERY_TYPE;
--- MiniDeliveryManagementSystem/ExpressDelivery.h
+++ MiniDeliveryManagementSystem/ExpressDelivery.h
@@ -5,6 +5,21 @@
 class ExpressDelivery : public Delivery
 {
 public:
+	ExpressDelivery(
+		std::string idParam,
+		std::string customerNameParam,
+		std::string sourceCityParam,
+		std::string destinationCityParam,
+		double weightParam,
+		Status statusParam
+	);
+
+	ExpressDelivery(const ExpressDelivery& other);
+
+	ExpressDelivery& operator=(const ExpressDelivery& other);
+
+	virtual ~ExpressDelivery();
+
 	virtual DeliveryType getDeliveryType() const;
 };
 
