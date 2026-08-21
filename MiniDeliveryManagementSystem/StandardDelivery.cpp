9f68dcd4374a0cddc4a437ec0567419023a4d843
CRISTIAN <crpopa1@sheffield.ac.uk>
2026-08-21 01:47:48 +0100

    Add Standard Delivery Concrete Product constructors and destructor using rule of 4

--- MiniDeliveryManagementSystem/StandardDelivery.cpp
+++ MiniDeliveryManagementSystem/StandardDelivery.cpp
@@ -1,6 +1,61 @@
 #include "StandardDelivery.h"
 #include "DeliveryConstants.h"
 
+
+StandardDelivery::StandardDelivery(
+    std::string idParam,
+    std::string customerNameParam,
+    std::string sourceCityParam,
+    std::string destinationCityParam,
+    double weightParam,
+    Status statusParam
+):
+    Delivery(
+        idParam,
+        customerNameParam,
+        sourceCityParam,
+        destinationCityParam,
+        weightParam,
+        statusParam
+    )
+{
+}
+
+
+StandardDelivery::StandardDelivery(const StandardDelivery& other):
+    Delivery(
+        other.getId(),
+        other.getCustomerName(),
+        other.getSourceCity(),
+        other.getDestinationCity(),
+        other.getWeight(),
+        other.getStatus()
+    )
+{
+}
+
+
+StandardDelivery& StandardDelivery::operator=(const StandardDelivery& other)
+{
+    if (this != &other)
+    {
+        setId(other.getId());
+        setCustomerName(other.getCustomerName());
+        setSourceCity(other.getSourceCity());
+        setDestinationCity(other.getDestinationCity());
+        setWeight(other.getWeight());
+        setStatus(other.getStatus());
+    }
+
+    return *this;
+}
+
+
+StandardDelivery::~StandardDelivery()
+{
+}
+
+
 DeliveryType StandardDelivery::getDeliveryType() const
 {
     return STANDARD_DELIVERY_TYPE;
--- MiniDeliveryManagementSystem/StandardDelivery.h
+++ MiniDeliveryManagementSystem/StandardDelivery.h
@@ -5,6 +5,21 @@
 class StandardDelivery: public Delivery
 {
 public:
+	StandardDelivery(
+		std::string idParam,
+		std::string customerNameParam,
+		std::string sourceCityParam,
+		std::string destinationCityParam,
+		double weightParam,
+		Status statusParam
+	);
+
+	StandardDelivery(const StandardDelivery& other);
+
+	StandardDelivery& operator=(const StandardDelivery& other);
+
+	virtual ~StandardDelivery();
+
 	virtual DeliveryType getDeliveryType() const;
 };
 
